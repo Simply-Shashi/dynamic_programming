4452830469a482743e4fbbf4371a1e5a20f291d8
Simply-Shashi <ethicalhacker605@gmail.com>
2020-10-27 06:58:13 +0530

    equal_sum_partition

--- equalsum.cpp
+++ equalsum.cpp
@@ -1,36 +1,52 @@
 #include <iostream>
 using namespace std;
 
-bool subsetsum(int arr[], int sum, int n)
+class equalsumpartition
 {
-    bool t[5][12];
-    if (sum == 0 || n == 0)
-        return false;
-    for (int i = 0; i < n + 1; i++)
-        for (int j = 0; j < sum + 1; j++)
+public:
+    bool equalsum(int arr[], int n)
+    {
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum = sum + arr[i];
+        if (sum % 2 != 0)
+            return false;
+        else if (sum % 2 == 0)
+            return subsetsum(arr, sum / 2, n);
+    }
+    bool subsetsum(int arr[], int sum, int n)
+    {
+        bool t[n + 1][sum + 1];
+        if (sum == 0 || n == 0)
+            return false;
+        for (int i = 0; i < n + 1; i++)
         {
-            if (i == 0)
-                t[i][j] = false;
-            if (j == 0)
-                t[i][j] = true;
-
-            if (arr[i - 1] <= j)
-                t[i][j] = t[i][j - arr[i - 1]] || t[i - 1][j];
-            else
-                t[i][j] = t[i - 1][j];
+            for (int j = 0; j < sum + 1; j++)
+            {
+                if (i == 0)
+                    t[i][j] = false;
+                if (j == 0)
+                    t[i][j] = true;
+            }
         }
-    return t[5][12];
-}
+        for (int i = 1; i < n + 1; i++)
+        {
+            for (int j = 1; j < sum + 1; j++)
+            {
+                if (arr[i - 1] <= j)
+                    t[i][j] = t[i - 1][j - arr[i - 1]] || t[i - 1][j];
+                else
+                    t[i][j] = t[i - 1][j];
+            }
+        }
+        return t[n][sum];
+    }
+};
 
 int main()
 {
-    int arr[] = {1, 5, 11, 5};
+    int arr[] = {2, 8, 7, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-        arr[i] += sum;
-    if (sum % 2 != 0)
-        return false;
-    else if (sum % 2 == 0)
-        return subsetsum(arr, sum / 2, n);
+    equalsumpartition ob;
+    cout << ob.equalsum(arr, n);
 }
